d23a8bf5337de1dfbbdf7ed82b09bbd2d393d0f4
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2025-10-24 16:02:45 +0400

    change title

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ import 'swiper/css/free-mode';
 import { dM_Sans, } from '@/lib/font'
 import { Toaster } from 'react-hot-toast';
 export const metadata = {
-    title: 'Dubai Creek Harbour EN - EMAAR',
+    title: 'Dubai Hills Estate EN - EMAAR',
     description: 'Project by Emaar',
 }
 
